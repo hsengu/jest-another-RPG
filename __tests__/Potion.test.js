c8c5eb8e1984a01b17459f6dc7b64013f812aad0
Hok S. Uy <hoksuy@gmail.com>
2021-08-18 22:35:28 -0700

    added player health check methods

--- __tests__/Potion.test.js
+++ __tests__/Potion.test.js
@@ -1,11 +1,5 @@
 const { TestWatcher } = require('jest');
-const randomNumber = require('../lib/random.js');
 const Potion = require('../lib/Potion.js');
-
-test('gets random number between 1 and 10', () => {
-    expect(randomNumber()).toBeGreaterThanOrEqual(1);
-    expect(randomNumber()).toBeLessThanOrEqual(10);
-});
 
 test('creates a random potion object', () => {
     const potion = new Potion();
